1600bb27d6599281e8b52ecbf8acc4b4afba15a6
Nicolas VINCENT <nicolas.vincent@sogeti.com>
2015-03-30 11:09:03 +0200

    ADA : compute mean value

--- mean.adb
+++ mean.adb
@@ -3,10 +3,10 @@ use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO ;
 
 procedure mean is
   n : constant natural:=5;
-  subtype t_grade is natural range 0..20;
+  subtype t_grade is float range 0.0..20.0;
   type t_grades is array(1..n) of t_grade;
   grades : t_grades;
-  sum : natural:=0;
+  sum : float:=0.0;
 begin
   for i in grades'range loop
     put("grade student "); put(i, width=>0); put(" : ");
@@ -16,7 +16,5 @@ begin
   for i in grades'range loop
     sum := sum + grades(i);
   end loop;
-  put(sum/grades'length);
-
-
+  put(sum/float(grades'length), fore=>2, aft=>2, exp=>0);
 end mean;
